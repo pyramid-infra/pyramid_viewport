e4c216ad9572a08e230982eef8d5dd9f5da0ca5e
Fredrik Noren <fredrik.jw.noren@gmail.com>
2015-09-07 23:34:21 -0400

    Remove dynamic duration in fps counter

--- src/fps_counter.rs
+++ src/fps_counter.rs
@@ -5,8 +5,7 @@ use time::*;
 pub struct FpsCounter {
     elapsed: Duration,
     n_frames: i32,
-    fps: f32,
-    update_period: Duration
+    fps: f32
 }
 
 impl FpsCounter {
@@ -14,22 +13,16 @@ impl FpsCounter {
         FpsCounter {
             elapsed: Duration::zero(),
             n_frames: 0,
-            fps: 0.0,
-            update_period: Duration::milliseconds(1000)
+            fps: 0.0
         }
     }
     pub fn add_frame(&mut self, duration: Duration) {
         self.n_frames += 1;
         self.elapsed = self.elapsed + duration;
-        if (self.elapsed >= self.update_period) {
+        if self.elapsed >= Duration::seconds(1) {
             self.fps = 1000.0 * self.n_frames as f32 / (self.elapsed.num_milliseconds() as f32);
             self.elapsed = Duration::zero();
             self.n_frames = 0;
-        }
-        if self.fps < 10.0 {
-            self.update_period = Duration::seconds(10);
-        } else {
-            self.update_period = Duration::seconds(1);
         }
     }
 }
