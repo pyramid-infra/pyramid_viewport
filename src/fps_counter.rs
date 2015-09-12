9b88318c4bbdba2946594070c93d77d9e2a67daa
Fredrik Noren <fredrik.jw.noren@gmail.com>
2015-09-12 01:17:20 -0400

    Show total frames in fps counter

--- src/fps_counter.rs
+++ src/fps_counter.rs
@@ -5,6 +5,7 @@ use time::*;
 pub struct FpsCounter {
     elapsed: Duration,
     n_frames: i32,
+    n_total_frames: i32,
     fps: f32
 }
 
@@ -13,11 +14,13 @@ impl FpsCounter {
         FpsCounter {
             elapsed: Duration::zero(),
             n_frames: 0,
+            n_total_frames: 0,
             fps: 0.0
         }
     }
     pub fn add_frame(&mut self, duration: Duration) {
         self.n_frames += 1;
+        self.n_total_frames += 1;
         self.elapsed = self.elapsed + duration;
         if self.elapsed >= Duration::seconds(1) {
             self.fps = 1000.0 * self.n_frames as f32 / (self.elapsed.num_milliseconds() as f32);
@@ -29,10 +32,14 @@ impl FpsCounter {
 
 impl ToString for FpsCounter {
     fn to_string(&self) -> String {
+        let tot = match self.n_total_frames < 1000 {
+            true => format!("#{}", self.n_total_frames),
+            false => "".to_string()
+        };
         if self.fps < 10.0 {
-            format!("{:.3} fps", self.fps)
+            format!("{:.3} fps {}", self.fps, tot)
         } else {
-            format!("{:.0} fps", self.fps)
+            format!("{:.0} fps {}", self.fps, tot)
         }
     }
 }
